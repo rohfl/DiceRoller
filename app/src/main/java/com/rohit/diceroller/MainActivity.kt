23bea2f855093b1040fdd7417ecfee61ed3dde83
Rohit Jangid <iamrohitjangid21@gmail.com>
2020-10-18 11:36:51 +0530

    Some Error Fixes.

--- app/src/main/java/com/rohit/diceroller/MainActivity.kt
+++ app/src/main/java/com/rohit/diceroller/MainActivity.kt
@@ -2,10 +2,30 @@ package com.rohit.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button : Button = findViewById(R.id.roll)
+        button.setOnClickListener { rollIt() }
+    }
+    private fun rollIt() {
+        var randNo : Int? = Random.nextInt(6) + 1
+            var diceImage : ImageView = findViewById(R.id.dice)
+            var setNo  = when(randNo) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        diceImage.setImageDrawable(ContextCompat.getDrawable(this, setNo));
     }
 }
